2168e892f094005240fa3416d350b19a05aabbc3
Zohaib Tariq <se.zohaib@gmail.com>
2022-09-02 00:35:20 +0500

    removed default credentials

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -12,12 +12,9 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  // KeenThemes mock, change it to:
   defaultAuth: any = {
-    "email": "admin@admin.com",
-    "password": "sadmin123"
-    // email: 'admin@demo.com',
-    // password: 'demo',
+    "email": "",
+    "password": ""
   };
   loginForm: FormGroup;
   hasError: boolean;
